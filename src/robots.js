10ea5c1d627fb57054addd6d2b7890686a012d22
DELL <dell@ubuntu.ubuntu-domain>
2020-05-09 17:38:58 +0530

    Day17

---
+++ src/robots.js
@@ -0,0 +1,20 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'xyz',
+        username: 'xyz',
+        emaill: 'xyz@abc.com'
+    },
+    {
+        id: 2,
+        name: 'abcc',
+        username: 'xyz',
+        emaill: 'xyz@abc.com'
+    },
+    {
+        id: 3,
+        name: 'ijjkkk',
+        username: 'xyz',
+        emaill: 'xyz@abc.com'
+    }
+]
